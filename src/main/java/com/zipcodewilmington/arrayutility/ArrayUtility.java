5e6b978c0ca91b7dc28a990dc032a592664f09c9
Chung Arguello <chung@zipcoders-MacBook-Pro-90.local>
2019-11-15 09:53:02 -0500

    almost done

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,7 +1,45 @@
 package com.zipcodewilmington.arrayutility;
 
+
+
+import javax.xml.stream.StreamFilter;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
  * Created by leon on 3/6/18.
  */
-public class ArrayUtility {
+public class ArrayUtility<T>  {
+
+    private T[] data;
+
+    public ArrayUtility(T[] data) {
+        this.data = data;
+    }
+
+    public Integer countDuplicatesInMerge(T[] arrayToMerge, T valueToEvaluate){
+
+        return Stream.concat(Stream.of(data),Stream.of(arrayToMerge)).collect(Collectors.toList()).stream()
+                .filter(i -> i == valueToEvaluate).collect(Collectors.toList()).size();
+    }
+
+    public T getMostCommonFromMerge(T[] arrayToMerge){
+
+        return null;
+    }
+
+    public T[] removeValue(T valueToRemove){
+
+        return null;
+    }
+
+    public Integer getNumberOfOccurrences(T valueToEvaluate){
+
+        return null;
+    }
 }
